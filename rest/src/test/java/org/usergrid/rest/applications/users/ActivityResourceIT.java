ce197e09b41541ed8777257f281137517c1c6a97
Dave Johnson <dmjohnson@apigee.com>
2013-11-21 15:43:28 -0500

    Fixing tests broken by error handling changes.

--- rest/src/test/java/org/usergrid/rest/applications/users/ActivityResourceIT.java
+++ rest/src/test/java/org/usergrid/rest/applications/users/ActivityResourceIT.java
@@ -66,7 +66,9 @@ public class ActivityResourceIT extends AbstractRestIT {
 
         boolean fail = false;
         try {
-            client.postGroupActivity( GROUP, null );
+            ApiResponse groupActivity = client.postGroupActivity( GROUP, null );
+            fail = (groupActivity.getError() != null);
+            
         }
         catch ( Exception e ) {
             fail = true;
